dea0d0ad850f8c2fe359ba0c128474b56a2e8c4b
ChristinaPenheiro <alizacyndi@gmail.com>
2026-09-26 09:50:15 +0600

    dark bg error solved

--- src/components/shared/NavButton.tsx
+++ src/components/shared/NavButton.tsx
@@ -42,7 +42,7 @@ const NavButton = () => {
                 <svg
                   aria-label="Menu"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
+                  className="h-5 w-5 text-white"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
